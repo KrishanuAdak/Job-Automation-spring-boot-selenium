0f3b2fa5753c90920a6f56253b310c8b633ec26d
Krishanu <iamkrishanu@Krishanus-MacBook-Air.local>
2026-03-21 22:48:26 +0530

    Command Line Runner Added

--- src/main/java/com/example/job_automation/JobAutomationApplication.java
+++ src/main/java/com/example/job_automation/JobAutomationApplication.java
@@ -1,7 +1,12 @@
 package com.example.job_automation;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.job_automation.service.JobAutomationService;
+import com.example.job_automation.service.LoginService;
 
 @SpringBootApplication
 public class JobAutomationApplication {
@@ -11,6 +16,14 @@ public class JobAutomationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobAutomationApplication.class, args);
+		
 	}
+	    @Bean
+    public CommandLineRunner run(LoginService loginService, JobAutomationService jobAutomationService) {
+        return args -> {
+            loginService.login();
+            jobAutomationService.processJobs();
+        };
+    }
 
 }
